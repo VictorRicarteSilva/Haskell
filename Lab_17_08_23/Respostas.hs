70f7812a70b9ca6d888d0a00d8b9b03066918e7d
VictorRicarteSilva <victorricarte155@gmail.com>
2023-08-28 16:32:09 -0300

    Lista02 quase finalizada

--- Lab_17_08_23/Respostas.hs
+++ Lab_17_08_23/Respostas.hs
@@ -96,7 +96,7 @@ areaRetangulo :: Int -> Int -> Int
 areaRetangulo base altura = base * altura
 
 --6
-areaQuadrado :: Int -> Int
+areaQuadrado :: Float -> Float
 areaQuadrado lado = lado * lado
 
 --7
@@ -122,4 +122,62 @@ volumeCubo :: Float -> Float
 volumeCubo aresta = aresta ^ 3
 
 --12
+volumeParalelepipedo :: Float -> Float -> Float -> Float
+volumeParalelepipedo comprimento largura altura = comprimento * largura * altura
 
+--13
+areaPiramideQuadrada :: Float -> Float -> Float -> Float
+areaPiramideQuadrada areabase base altura = areaQuadrado areabase + areaTriangulo base altura
+
+--14
+volumeEsfera :: Float -> Float
+volumeEsfera raio = (4.0 * pi * raio ^ 3) / 3.0
+
+--15
+hipotenusa :: Float -> Float -> Float
+hipotenusa oposto adjacente = sqrt((oposto ^ 2) + (adjacente ^ 2))
+
+--16
+distancia0 :: Float -> Float -> Float
+distancia0 x y = sqrt((x ^ 2) + (y ^ 2))
+
+--17
+delta :: Float -> Float -> Float
+delta maior menor = if maior > menor then
+                    maior - menor else
+                    menor - maior
+quadradoF :: Float -> Float
+quadradoF numero = numero * numero
+
+distancia :: Float -> Float -> Float -> Float -> Float
+distancia x1 y1 x2 y2 = sqrt (quadradoF (delta x1 x2) + quadradoF (delta y1 y2))
+
+--18
+cubo :: Int -> Int
+cubo numero = numero ^ 3
+
+--19
+quartaPotencia :: Int -> Int
+quartaPotencia numero = cubo numero * numero
+
+--20
+segundosParaHora :: Int -> Int
+segundosParaHora segundos = div segundos 3600
+
+--21
+segundosParaMinuto :: Int -> Int
+segundosParaMinuto segundos = div segundos 60
+
+--22
+fahrenheitParaCelsius :: Float -> Float
+fahrenheitParaCelsius fahrenheit = (fahrenheit - 32) / 1.8
+
+--23
+kelvinParaCelsius :: Float -> Float
+kelvinParaCelsius tempKelvin = tempKelvin - 273.15
+
+--24
+fahrenheitParaKelvin :: Float -> Float
+fahrenheitParaKelvin fahrenheit = fahrenheitParaCelsius fahrenheit + 272.15
+
+--25
